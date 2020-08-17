b21077bdd50347d3aca946ef3b18c6fd2654b65a
Marko Bisevac <54064471+MarkoB-hub@users.noreply.github.com>
2020-08-17 11:40:04 +0200

    added premake file

---
+++ premake5.lua
@@ -0,0 +1,30 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    
+	targetdir "../../temp/"
+	objdir "../../temp/"
+
+	files
+	{
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
+    
+	buildoptions {"-fPIC", "-g"}
+
+	filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+        staticruntime "On"
+        
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
